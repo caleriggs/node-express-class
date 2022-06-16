1367cc58c9f364412d17587100a5842ebe60bc02
Cale Riggs <riggs.cale@gmail.com>
2022-06-16 18:43:23 -0500

    changes

--- v1/serviceClients/weatherService.js
+++ v1/serviceClients/weatherService.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { apiKey } from "../../env.js";
 
 const lat = 38.624576836125634;
 const lon = -90.18495815951067;
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=YOUR_ID_HERE`;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
 export const weatherServiceClient = async () => {
   const result = await axios

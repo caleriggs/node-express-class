2eb12c6656447f0fd952655e72b1060e58f67fd3
Cale Riggs <riggs.cale@gmail.com>
2022-06-16 16:49:58 -0500

    removed api key

--- v1/serviceClients/weatherService.js
+++ v1/serviceClients/weatherService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const lat = 38.624576836125634;
 const lon = -90.18495815951067;
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3790dc45273698a12e774374079ac64f`;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=YOUR_ID_HERE`;
 
 export const weatherServiceClient = async () => {
   const result = await axios

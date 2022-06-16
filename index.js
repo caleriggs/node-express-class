9a5f7e15ac5bcbe8d228af383b7674318e08beab
Cale Riggs <riggs.cale@gmail.com>
2022-06-16 16:41:38 -0500

    start here

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import { weatherServiceClient } from "./v1/serviceClients/weatherService.js";
+
+const app = express();
+const port = 1138;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("OK"));
+
+app.listen(port, () =>
+  console.log("App listening on : ", `http://localhost:${port}`)
+);
---
+++ v1/serviceClients/weatherService.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const lat = 38.624576836125634;
+const lon = -90.18495815951067;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3790dc45273698a12e774374079ac64f`;
+
+export const weatherServiceClient = async () => {
+  const result = await axios
+    .get(url)
+    .then((response) => response.data)
+    .catch((err) => console.error(err));
+
+  return result;
+};
